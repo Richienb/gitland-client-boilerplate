1b08ef3395c5d948a0f4fa3e5a40d97b348f7926
Simon Lacroix <simon@miaou.be>
2020-04-29 17:27:13 +0200

    Update random_action.rb

--- random_action.rb
+++ random_action.rb
@@ -1,7 +1,12 @@
 #! ruby
 
-direction = %w[right left up down].sample
-File.open('act', 'w') { |file| file.write(direction) }
-system('git add -A')
-system('git commit -m "New move"')
-system('git push')
+loop do
+  direction = %w[right left up down].sample
+  File.open('act', 'w') { |file| file.write(direction) }
+
+  system('git add -A')
+  system('git commit -m "New move"')
+  system('git push')
+
+  sleep 60
+end
