0b6872ad47a4b80e3af5bffb89d156c3b652c9f3
Simon Lacroix <simon@miaou.be>
2020-04-29 19:14:17 +0200

    Update random_action.rb

--- random_action.rb
+++ random_action.rb
@@ -5,7 +5,7 @@ loop do
   File.open('act', 'w') { |file| file.write(direction) }
 
   system('git add -A')
-  system('git commit -m "New move"')
+  system('git commit -m "Next move"')
   system('git push')
 
   sleep 60
